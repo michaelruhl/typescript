d697b3551587d50bd14a166b7335f440f8824ef1
Michael Ruhl <michaelruhl14@hotmail.com>
2022-11-24 08:20:31 -0500

    udemy course

--- src/index.ts
+++ src/index.ts
@@ -17,3 +17,95 @@ class Queue<T> {
 
 let queue: Queue<number> = new Queue();
 
+// object types & aliases
+type Point = { x: number, y: number }
+
+let center: Point = { x: number, y: number } = {
+    x: 0
+    y: 0
+}
+
+let unit: Point = { x: number, y: number } = {
+    x: 1,
+    y: 1,
+}
+
+const point: Point = { x: 0, y: 0 }
+
+point = { x: 1, y: 1 }
+
+point.x = 123
+point.y = 456
+
+function add(a: number, b: number): number {
+    return a + b
+}
+
+function log(message: string): void {
+    console.log('LOG:', message)
+}
+
+function sum(...values: number[]) {
+    return values.reduce((previous, current) => {
+        return previous + current
+    })
+}
+
+sum(1, 2)
+sum(1, 2, 3)
+
+type Add = (a: number, b: number) => number
+
+let add Add
+
+add = function (a: number, b: number): number {
+    return a + b
+}
+
+add = (a, b) => a + b
+
+type user = { id: string }
+type product = { id: string }
+
+let user: User = { id: 'user-asfef23' }
+let product: Product = { id: 'product-fwefw575' }
+
+type Point2D = { x: number, y: number }
+type Point3D = { x: number, y: number, z: number }
+
+let point2D: Point2D = { x: 0, y: 10 }
+let point3d: Point3D = { x: 0, y: 10, z: 20 }
+
+
+// Extra info ok
+point2D = point3d
+function takesPoint2D(point: Point2D) { }
+takesPoint2D(point3D)
+
+// Error: missing info
+point3D = point2D
+function takesPoint3D(point: Point3D) {  }
+takesPoint3D(point2D)
+
+class Animal {
+    private name: string
+
+    constructor(name: string) {
+        this.name = name
+    }
+
+    public move(distanceinMeters: number) void{
+        console.log(`${this.name} moves ${distanceInMeters}m.`)
+    }
+}
+
+let cat = new Animal('Cat')
+cat.move(10)
+cat.name = 'Dog'
+
+class Bird extends Animal {
+    fly(distanceInMeters: number) {
+        console.log(`${this.name} flew ${distanceInMeters}m.`)
+
+    }
+}
---
+++ src/index1.ts
@@ -0,0 +1,19 @@
+// Array
+let array: number[] = [1, 2, 3];
+
+
+// Usage
+array= [1];
+array = [1, 2, 3, 4, 5];
+array = ['hello']; 
+
+
+// Tuple
+let tuple: [number, number] = [0, 0]
+
+// Usage
+tuple = [1, 1]
+tuple = [1, 6]
+tuple = [5]
+tuple = [5, 4, 3]
+tuple = ['elite', 1337]
